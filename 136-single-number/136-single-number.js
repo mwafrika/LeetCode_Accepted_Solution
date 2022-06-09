485fbc6d6b548d5107e7246f2f6bbfe2aff2543d
MWAFRIKA MUFUNGIZI <mwafrikajosue@gmail.com>
2022-06-09 09:17:23 +0200

    Time: 67 ms (94.77%), Space: 44.7 MB (63.94%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -8,7 +8,7 @@ var singleNumber = function(nums) {
     
     for(let i=0;i<sortedArray.length; i+=2){
         
-        if(i===sortedArray.length || sortedArray[i] !== sortedArray[i+1]){
+        if(sortedArray[i] !== sortedArray[i+1]){
             
             return sortedArray[i]
         }
