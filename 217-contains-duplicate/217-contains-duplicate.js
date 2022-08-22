cb99388e1ff9c960ac97716edbca3264e954d01f
MWAFRIKA MUFUNGIZI <mwafrikajosue@gmail.com>
2022-08-22 10:32:49 +0300

    Time: 2564 ms (5.97%), Space: 48.3 MB (94.08%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -5,11 +5,5 @@
 var containsDuplicate = function(nums) {
     
     const duplicates = (item, index) => nums.indexOf(item) !== index
-    
-    const hh = nums.some(duplicates)
-    if(hh){
-       return true
-    }else{
-        return false
-    }
+    return nums.some(duplicates) ? true : false
 };
