749adf01729cac46b5e34016e3a2a91c28f6a675
MWAFRIKA MUFUNGIZI <mwafrikajosue@gmail.com>
2022-08-23 11:26:46 +0300

    Time: 211 ms (19.84%), Space: 42.5 MB (68.64%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,15 +4,13 @@
  * @return {number[]}
  */
 var twoSum = (nums, target) =>{
-   let acc = [];
-    
-    for(let i=0; i<nums.length; i++){
-         for(let j=i+1; j<nums.length; j++){
-             if(nums[i] + nums[j] === target){
-                    acc.push(i)
-                    acc.push(j)
-             }
-         }
-    }
-    return acc
+    let count = [];
+  for(let i = 0; i<nums.length; i++){
+      for(let j = i+1; j<nums.length; j++){
+          if(nums[i]+ nums[j] === target){
+              count.push(i,j)
+          }
+      }
+  }
+    return count
 };
