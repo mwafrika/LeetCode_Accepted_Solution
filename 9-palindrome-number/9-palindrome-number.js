502119949038dc0830ca2580d9ae430ad79362a6
MWAFRIKA MUFUNGIZI <mwafrikajosue@gmail.com>
2022-08-23 15:19:14 +0300

    Time: 300 ms (36.94%), Space: 51.7 MB (13.37%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const rev = Number(x.toString().trim().split("").reverse().join(""))
+    const rev = Number(x.toString().split("").reverse().join(""))
     return rev === x ? true : false 
 };
